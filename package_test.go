6ac9adc428014e9f08fc27f1b261aec6b62e6f71
wara <kusabashira227@gmail.com>
2015-06-01 21:36:08 +0900

    Add first test for ToSouceURI

--- package_test.go
+++ package_test.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"testing"
+)
+
+var sourceURITests = []struct {
+	src string
+	dst string
+}{
+	{
+		"https://github.com/sunaku/vim-unbundle",
+		"https://github.com/sunaku/vim-unbundle",
+	},
+}
+
+func TestSourceURI(t *testing.T) {
+	for _, test := range sourceURITests {
+		expect := test.dst
+		actual, err := ToSourceURI(test.src)
+		if err != nil {
+			t.Errorf("ToSourceURI(%q) returns %q, want nil", err)
+		}
+		if actual != expect {
+			t.Errorf("%q: got %q, want %q",
+				test.src, actual, expect)
+		}
+	}
+}
