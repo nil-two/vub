2101f0a390cd1135611f34a9b996c1dbe7694d54
wara <kusabashira227@gmail.com>
2015-06-01 22:30:02 +0900

    Enable convert short GitHub URI

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"regexp"
+)
+
+var (
+	ShortGitHubURI = regexp.MustCompile(`^[\w\-.]+/[\w\-.]+$`)
+)
+
 func ToSourceURI(uri string) (string, error) {
-	return uri, nil
+	switch {
+	case ShortGitHubURI.MatchString(uri):
+		return "https://github.com/" + uri, nil
+	default:
+		return uri, nil
+	}
 }
