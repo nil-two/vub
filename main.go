0f1d5e8dcb9e6856c962e385bc6d3f677f07b68b
wara <kusabashira227@gmail.com>
2015-06-04 23:29:50 +0900

    Summarize error handling at the top level

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ Options:
 `[1:])
 }
 
-func main() {
+func _main() (code int, err error) {
 	var filetype string
 	flag.StringVar(&filetype, "f", "", "")
 	flag.StringVar(&filetype, "filetype", "", "")
@@ -54,39 +54,44 @@ func main() {
 	switch {
 	case isHelp:
 		usage()
-		os.Exit(0)
+		return 0, nil
 	case flag.NArg() < 1:
-		shortUsage()
-		os.Exit(2)
+		return 2, nil
 	case removeMode && updateMode:
-		fmt.Fprintln(os.Stderr, "vub:", "cannot specify multiple mode")
-		shortUsage()
-		os.Exit(2)
+		return 2, fmt.Errorf("cannot specify multiple mode")
 	}
 	uri := flag.Arg(0)
 
 	p, err := NewPackage(uri, filetype)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "vub:", err)
-		os.Exit(2)
+		return 1, err
 	}
 	p.Verbose(verbose)
 
 	switch {
 	case removeMode:
 		if err := p.Remove(os.Stdout); err != nil {
-			fmt.Fprintln(os.Stderr, "vub:", err)
-			os.Exit(1)
+			return 1, err
 		}
 	case updateMode:
 		if err := p.Update(os.Stdout); err != nil {
-			fmt.Fprintln(os.Stderr, "vub:", err)
-			os.Exit(1)
+			return 1, err
 		}
 	default:
 		if err := p.Install(os.Stdout); err != nil {
-			fmt.Fprintln(os.Stderr, "vub:", err)
-			os.Exit(1)
+			return 1, err
 		}
 	}
+	return 0, nil
+}
+
+func main() {
+	code, err := _main()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "vub:", err)
+	}
+	if code == 2 {
+		shortUsage()
+	}
+	os.Exit(code)
 }
